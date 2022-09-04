59bc840255a61cff3d08a5117892bb2ad05a6994
Ranjeethkumar <rmpranjeeth@gmail.com>
2022-09-04 11:57:05 +0530

    first commit

---
+++ src/components/ChartsPage/DonutChart.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Doughnut } from 'react-chartjs-2';
+import {Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend} from 'chart.js'
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    ArcElement,
+    Title,
+    Tooltip,
+    Legend
+  )
+
+const DonutChart = () => {
+  const data = {
+    labels: ['Direct', 'Referral', 'Social'],
+    datasets: [
+                {
+                  data: [55, 30, 15],
+                  backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+                  hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+                  hoverBorderColor: 'rgba(234, 236, 244, 1)',
+                  hoverOffset: 5,
+                  
+                },
+              ],
+   
+  }
+
+return (
+  <>
+  <Doughnut data={data}/>
+  </>
+)
+}
+
+export default DonutChart
